5e6dbe992f78a2d645a4bcb24832e349247d5598
Alisson Steffens <ali.steffens@gmail.com>
2020-11-18 22:35:33 -0300

    :art: add source to apis

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,21 @@ export default function Home() {
       <Head>
         <title>Awesome API</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-894PDP3V46"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-894PDP3V46');
+              `,
+          }}
+        />
       </Head>
 
       <main className={styles.main}>
@@ -15,31 +30,50 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Here you can see some of the best end-points ever created.
+          Here you can see some of the best end-points ever created (with random data to fill your apps).
         </p>
 
         <div className={styles.grid}>
-          <a href="/api/picsum" className={styles.card}>
-            <h3>Picsum &rarr;</h3>
+          <div className={styles.card}> 
+            <a href="/api/picsum"   className={styles.blue} id="picsum">
+              <h3>Picsum &rarr;</h3>
+            </a>
             <p>Get a random curated image from Unsplash, with its details.</p>
-          </a>
+            <p className={styles.fonte}><strong>Source:</strong> <a href="https://picsum.photos/" className={styles.blue} target="_blank">Picsum</a></p>
+            
+          </div>
 
-           <a href="/api/art" className={styles.card}>
-            <h3>Art &rarr;</h3>
+          <div className={styles.card}> 
+            <a href="/api/art"  className={styles.red} id="picsum">
+              <h3>Art &rarr;</h3>
+            </a>
             <p>Get a random image from the MET Museum, with its details.</p>
-          </a>
-          <a href="/api/rick-and-morty" className={styles.card}>
-            <h3>Rick and Morty &rarr;</h3>
+            <p className={styles.fonte}><strong>Source:</strong> <a href="https://www.metmuseum.org/" className={styles.red} target="_blank">MET Museum</a></p>
+          </div>
+
+          <div className={styles.card}> 
+            <a href="/api/rick-and-morty"  className={styles.green} id="picsum">
+              <h3>Rick and Morty &rarr;</h3>
+            </a>
             <p>Get a random character from Rick and Morty, with its details.</p>
-          </a>
-          <a href="/api/final-space" className={styles.card}>
-            <h3>Final Space &rarr;</h3>
+            <p className={styles.fonte}><strong>Source:</strong> <a href="https://rickandmortyapi.com/" className={styles.green} target="_blank">Rick and Morty API</a></p>
+          </div>
+
+          <div className={styles.card}> 
+            <a href="/api/final-space"  className={styles.purple} id="picsum">
+              <h3>Final Space &rarr;</h3>
+            </a>
             <p>Get a random character from Final Space, with its details.</p>
-          </a>
-          <a href="/api/cors?image=https://images.metmuseum.org/CRDImages/ep/original/DT2722.jpg" className={styles.card} >
-            <h3>Cors &rarr;</h3>
+            <p className={styles.fonte}><strong>Source:</strong> <a href="https://finalspaceapi.com/" className={styles.purple} target="_blank">Final Space API</a></p>
+          </div>
+
+          <div className={styles.card}> 
+            <a href="/api/cors?image=https://images.metmuseum.org/CRDImages/ep/original/DT2722.jpg"  className={styles.blue} id="picsum">
+              <h3>Cors &rarr;</h3>
+            </a>
             <p>Cors Anywhere for images</p>
-          </a>
+          </div>
+
           {/*
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
