61a12d661916bfe1a5d9d1cb7c82241445b9f8af
Alisson Steffens <ali.steffens@gmail.com>
2021-04-14 01:43:20 -0300

    :bug: add cors header

--- pages/api/earthview.js
+++ pages/api/earthview.js
@@ -1,18 +1,19 @@
 import endpoints from '../../data/satellite_images.json'
 
 export default (req, res) => {
-  res.statusCode = 200
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.statusCode = 200;
   // res.json(endpoints)
   let temp = endpoints[(Math.random() * endpoints.length) | 0]
   var out = {};
   let name = ""
-  if(temp.region !== ""){
+  if (temp.region !== "") {
     name = `${temp.region} - ${temp.country}`
-  } else{
+  } else {
     name = temp.country
   }
   out.title = name;
-  out.img = 'https://awesome-api.vercel.app/api/cors?image='+temp.image;
+  out.img = 'https://awesome-api.vercel.app/api/cors?image=' + temp.image;
   out.source = temp.map;
   res.json(out);
 }
