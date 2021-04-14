9338e39398ff44c54fcdf084a59378d4a3581de5
Alisson Steffens <ali.steffens@gmail.com>
2021-04-14 01:29:24 -0300

    :tada: add earth view

---
+++ pages/api/earthview.js
@@ -0,0 +1,18 @@
+import endpoints from '../../data/satellite_images.json'
+
+export default (req, res) => {
+  res.statusCode = 200
+  // res.json(endpoints)
+  let temp = endpoints[(Math.random() * endpoints.length) | 0]
+  var out = {};
+  let name = ""
+  if(temp.region !== ""){
+    name = `${temp.region} - ${temp.country}`
+  } else{
+    name = temp.country
+  }
+  out.title = name;
+  out.img = temp.image;
+  out.source = temp.map;
+  res.json(out);
+}
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,5 @@
 import endpoints from '../../data/endpoints'
 export default (req, res) => {
-    res.statusCode = 200
-    res.json(endpoints)
-  }
-  
+  res.statusCode = 200
+  res.json(endpoints)
+}
