e7e1efa1f6058063ba602f82cde1616e259a1bd0
Alisson Steffens <ali.steffens@gmail.com>
2021-04-14 01:38:24 -0300

    :bug: fix cors

--- pages/api/earthview.js
+++ pages/api/earthview.js
@@ -12,7 +12,7 @@ export default (req, res) => {
     name = temp.country
   }
   out.title = name;
-  out.img = temp.image;
+  out.img = 'https://awesome-api.vercel.app/api/cors?' + temp.image;
   out.source = temp.map;
   res.json(out);
 }
